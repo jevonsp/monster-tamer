e6bc9abc494b131721b9ce2a6173f7247f40fc76
jevon <pineapplesituation@gmail.com>
2026-02-12 11:22:54 -0800

    Deepseek Reorg
    Crushed the line count from 358 to 302 in an effort to make Battle.gd a bit shorter

--- battle/battle.gd
+++ battle/battle.gd
@@ -2,6 +2,11 @@ extends CanvasLayer
 
 enum VisibilityState { OPTIONS, MOVES }
 
+const FOCUS_DEFAULTS = {
+	OPTIONS = 1,
+	MOVES = 1
+}
+
 var vis_state: VisibilityState = VisibilityState.OPTIONS
 var processing: bool = false
 
@@ -9,27 +14,36 @@ var player_actor: Monster
 var enemy_actor: Monster
 var player_party: Array[Monster] = []
 var enemy_party: Array[Monster] = []
+var turn_queue: Array[Dictionary] = []
 
-var turn_queue: Array = []
-
-var _last_focused_option: int = 1
-var _last_focused_move: int = 1
+var _last_focused: Dictionary = {
+	VisibilityState.OPTIONS: 1,
+	VisibilityState.MOVES: 1
+}
 
 #region NODE REFERENCES
 @onready var option_buttons_grid: GridContainer = $Content/OptionButtons
 @onready var move_buttons_grid: GridContainer = $Content/MoveButtons
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-@onready var player_level_label: Label = $Content/PlayerLevelLabel
-@onready var player_name_label: Label = $Content/PlayerNameLabel
-@onready var player_texture_rect: TextureRect = $Content/PlayerTextureRect
-@onready var player_hp_bar: ProgressBar = $Content/PlayerHPBar
-@onready var player_exp_bar: ProgressBar = $Content/PlayerEXPBar
+@onready var player_labels: Dictionary = {
+	level = $Content/PlayerLevelLabel,
+	name = $Content/PlayerNameLabel
+}
+@onready var player_display: Dictionary = {
+	texture = $Content/PlayerTextureRect,
+	hp_bar = $Content/PlayerHPBar,
+	exp_bar = $Content/PlayerEXPBar
+}
 
-@onready var enemy_level_label: Label = $Content/EnemyLevelLabel
-@onready var enemy_name_label: Label = $Content/EnemyNameLabel
-@onready var enemy_texture_rect: TextureRect = $Content/EnemyTextureRect
-@onready var enemy_hp_bar: ProgressBar = $Content/EnemyHPBar
+@onready var enemy_labels: Dictionary = {
+	level = $Content/EnemyLevelLabel,
+	name = $Content/EnemyNameLabel
+}
+@onready var enemy_display: Dictionary = {
+	texture = $Content/EnemyTextureRect,
+	hp_bar = $Content/EnemyHPBar
+}
 
 @onready var move_labels: Array[Label] = [
 	$Content/MoveButtons/Button0/Label,
@@ -44,27 +58,21 @@ func _ready() -> void:
 	_connect_signals()
 	_bind_buttons()
 
-
 func _unhandled_input(event: InputEvent) -> void:
-	if not event.is_action_pressed("no"):
-		return
-		
-	match vis_state:
-		VisibilityState.OPTIONS:
-			option_buttons_grid.get_children()[2].grab_focus()
-		VisibilityState.MOVES:
-			_change_vis_state(VisibilityState.OPTIONS)
-
+	if event.is_action_pressed("no"):
+		match vis_state:
+			VisibilityState.OPTIONS:
+				option_buttons_grid.get_child(2).grab_focus()
+			VisibilityState.MOVES:
+				_change_vis_state(VisibilityState.OPTIONS)
 
 func _connect_signals() -> void:
 	Global.send_player_party.connect(_set_player_party)
 	Global.wild_battle_requested.connect(_start_wild_battle)
 
-
 func _bind_buttons() -> void:
 	for button in get_tree().get_nodes_in_group("option_buttons"):
 		button.pressed.connect(_on_option_pressed.bind(button))
-	
 	for button in get_tree().get_nodes_in_group("move_buttons"):
 		button.pressed.connect(_on_move_pressed.bind(button))
 #endregion
@@ -73,49 +81,28 @@ func _bind_buttons() -> void:
 func _start_wild_battle(monster_data: MonsterData, level: int) -> void:
 	_clear_actors()
 	
-	var monster = monster_data.set_up(level)
-	_set_wild_party([monster])
-	set_enemy_actor()
+	var monster: Monster = monster_data.set_up(level)
+	enemy_party = [monster]
+	enemy_actor = enemy_party[0]
 	
-	set_player_actor()
+	player_actor = player_party[0]
+	player_actor.was_in_battle = true
 	
 	_toggle_player()
 	_display_current_monsters()
 	_toggle_visible()
-
 
 func end_battle() -> void:
 	_clear_all()
 	_toggle_visible()
 	_toggle_player()
 
-
 func _set_player_party(party: Array[Monster]) -> void:
 	player_party = party
-
-
-func _set_wild_party(party: Array[Monster]) -> void:
-	enemy_party = party
-	
-
-func set_player_actor(monster: Monster = null) -> void: 
-	if monster == null:
-		player_actor = player_party[0]
-	else:
-		player_actor = monster
-	player_actor.was_in_battle = true
-
-
-func set_enemy_actor(monster: Monster = null) -> void: 
-	if monster == null:
-		enemy_actor = enemy_party[0]
-	else:
-		enemy_actor = monster
 
 func _clear_actors() -> void:
 	player_actor = null
 	enemy_actor = null
-
 
 func _clear_all() -> void:
 	player_actor = null
@@ -127,17 +114,14 @@ func _clear_all() -> void:
 	processing = false
 #endregion
 
-#region # TURN EXECUTION
+#region TURN EXECUTION
 func _execute_player_turn(move: Move) -> void:
-	if not _add_move_to_queue(move, player_actor):
-		return
-	
-	processing = false
-	_manage_focus()
-	_get_enemy_action()
-	await _execute_turn_queue()
-	processing = true
-
+	if _add_move_to_queue(move, player_actor):
+		processing = false
+		_manage_focus()
+		_get_enemy_action()
+		await _execute_turn_queue()
+		processing = true
 
 func _get_enemy_action() -> void:
 	var available_moves: Array[Move] = []
@@ -145,21 +129,14 @@ func _get_enemy_action() -> void:
 		if move != null:
 			available_moves.append(move)
 	
-	if available_moves.is_empty():
-		return
-	
-	var move = available_moves.pick_random()
-	_add_move_to_queue(move, enemy_actor)
-
+	if not available_moves.is_empty():
+		_add_move_to_queue(available_moves.pick_random(), enemy_actor)
 
 func _add_move_to_queue(move: Move, actor: Monster) -> bool:
 	if move == null:
 		return false
 	
-	var target = enemy_actor if actor == player_actor else player_actor
-	if move.is_self_targeting:
-		target = actor
-	
+	var target: Monster = _get_target(actor, move)
 	turn_queue.append({
 		"action": move,
 		"actor": actor,
@@ -167,12 +144,16 @@ func _add_move_to_queue(move: Move, actor: Monster) -> bool:
 	})
 	return true
 
+func _get_target(actor: Monster, move: Move) -> Monster:
+	if move.is_self_targeting:
+		return actor
+	return enemy_actor if actor == player_actor else player_actor
 
 func _execute_turn_queue() -> void:
 	_sort_turn_queue()
 	
 	for entry in turn_queue:
-		var actor = entry.actor
+		var actor: Monster = entry.actor
 		var target: Monster = entry.target
 		var exp_completed = [false]
 		var on_exp_complete = func(): exp_completed[0] = true
@@ -182,16 +163,17 @@ func _execute_turn_queue() -> void:
 		if target and target.is_fainted and target == enemy_actor:
 			if not exp_completed[0]:
 				await Global.experience_animation_complete
-				print("got signal")
+		
 		if _check_win():
 			_win()
+			return
 		if _check_lose():
 			_lose()
+			return
 			
 	turn_queue.clear()
 	processing = true
 	_manage_focus()
-
 
 func _sort_turn_queue() -> void:
 	turn_queue.sort_custom(func(a, b): 
@@ -199,79 +181,66 @@ func _sort_turn_queue() -> void:
 			return a.action.priority > b.action.priority
 		return a.actor.speed > b.actor.speed
 	)
-	
+
 func _check_win() -> bool:
 	for monster in enemy_party:
 		if not monster.is_fainted:
 			return false
-	return true
-	
-	
+	return false
+
 func _check_lose() -> bool:
 	for monster in player_party:
 		if not monster.is_fainted:
-			return false 
-	return true
-	
-	
+			return false
+	return false
+
 func _win() -> void:
 	print("would win here")
-
 
 func _lose() -> void:
 	print("would lose here")
 #endregion
 
-#region # UI UPDATES
+#region UI UPDATES
 func _display_current_monsters() -> void:
-	_update_levels()
-	_update_names()
+	_update_labels()
 	_update_textures()
-	_update_hitpoints()
-	_update_exp()
+	_update_bars()
 	_update_moves()
 
-
-func _update_levels() -> void:
-	player_level_label.text = "Lvl. %s" % player_actor.level
-	enemy_level_label.text = "Lvl. %s" % enemy_actor.level
-
-
-func _update_names() -> void:
-	player_name_label.text = player_actor.name
-	enemy_name_label.text = enemy_actor.name
-
+func _update_labels() -> void:
+	player_labels.level.text = "Lvl. %s" % player_actor.level
+	player_labels.name.text = player_actor.name
+	enemy_labels.level.text = "Lvl. %s" % enemy_actor.level
+	enemy_labels.name.text = enemy_actor.name
 
 func _update_textures() -> void:
-	player_texture_rect.texture = player_actor.monster_data.texture
-	player_texture_rect.player_actor = player_actor # BIND
-	enemy_texture_rect.texture = enemy_actor.monster_data.texture
-	enemy_texture_rect.enemy_actor = enemy_actor # BIND
+	player_display.texture.texture = player_actor.monster_data.texture
+	player_display.texture.player_actor = player_actor
+	enemy_display.texture.texture = enemy_actor.monster_data.texture
+	enemy_display.texture.enemy_actor = enemy_actor
 
-
-func _update_hitpoints() -> void:
-	player_hp_bar.max_value = player_actor.max_hitpoints
-	player_hp_bar.value = player_actor.current_hitpoints
-	player_hp_bar.actor = player_actor # BIND
+func _update_bars() -> void:
+	# HP Bars
+	player_display.hp_bar.max_value = player_actor.max_hitpoints
+	player_display.hp_bar.value = player_actor.current_hitpoints
+	player_display.hp_bar.actor = player_actor
 	
-	enemy_hp_bar.max_value = enemy_actor.max_hitpoints
-	enemy_hp_bar.value = enemy_actor.current_hitpoints
-	enemy_hp_bar.actor = enemy_actor # BIND
-
-
-func _update_exp() -> void:
-	var max_exp = Monster.EXPERIENCE_PER_LEVEL * player_actor.level
-	var min_exp = Monster.EXPERIENCE_PER_LEVEL * (player_actor.level - 1)
+	enemy_display.hp_bar.max_value = enemy_actor.max_hitpoints
+	enemy_display.hp_bar.value = enemy_actor.current_hitpoints
+	enemy_display.hp_bar.actor = enemy_actor
 	
-	player_exp_bar.max_value = max_exp
-	player_exp_bar.min_value = min_exp
-	player_exp_bar.value = player_actor.experience
+	# EXP Bar
+	var max_exp: int = Monster.EXPERIENCE_PER_LEVEL * player_actor.level
+	var min_exp: int = Monster.EXPERIENCE_PER_LEVEL * (player_actor.level - 1)
 	
-	player_exp_bar.actor = player_actor
-
+	player_display.exp_bar.max_value = max_exp
+	player_display.exp_bar.min_value = min_exp
+	player_display.exp_bar.value = player_actor.experience
+	player_display.exp_bar.actor = player_actor
 
 func _update_moves() -> void:
-	for i in range(player_actor.moves.size()):
+	for i in player_actor.moves.size():
 		if player_actor.moves[i] != null:
 			move_labels[i].text = player_actor.moves[i].name
 
@@ -285,46 +254,24 @@ func _toggle_visible() -> void:
 		_focus_default()
 #endregion
 
-#region # UI FOCUS
+#region UI FOCUS
 func _change_vis_state(new_state: VisibilityState) -> void:
 	vis_state = new_state
-	
-	match new_state:
-		VisibilityState.OPTIONS:
-			move_buttons_grid.visible = false
-			option_buttons_grid.visible = true
-			_focus_last_used_option()
-		VisibilityState.MOVES:
-			option_buttons_grid.visible = false
-			move_buttons_grid.visible = true
-			_focus_last_used_move()
-
+	option_buttons_grid.visible = new_state == VisibilityState.OPTIONS
+	move_buttons_grid.visible = new_state == VisibilityState.MOVES
+	_focus_default()
 
 func _focus_default() -> void:
-	match vis_state:
-		VisibilityState.OPTIONS:
-			_focus_last_used_option()
-		VisibilityState.MOVES:
-			_focus_last_used_move()
-
-
-func _focus_last_used_option() -> void:
-	var children = option_buttons_grid.get_children()
-	if _last_focused_option < children.size():
-		children[_last_focused_option].grab_focus()
-
-
-func _focus_last_used_move() -> void:
-	var children = move_buttons_grid.get_children()
-	if _last_focused_move < children.size():
-		children[_last_focused_move].grab_focus()
-
+	var grid = option_buttons_grid if vis_state == VisibilityState.OPTIONS else move_buttons_grid
+	var index = _last_focused[vis_state]
+	var children = grid.get_children()
+	if index < children.size():
+		children[index].grab_focus()
 
 func _drop_focus() -> void:
 	var focused = get_viewport().gui_get_focus_owner()
 	if focused:
 		focused.release_focus()
-
 
 func _manage_focus() -> void:
 	if processing:
@@ -335,23 +282,20 @@ func _manage_focus() -> void:
 
 #region INPUT HANDLERS
 func _on_option_pressed(button: Button) -> void:
+	var index_map := {"Party": 0, "Fight": 1, "Run": 2, "Item": 3}
+	if button.name in index_map:
+		_last_focused[VisibilityState.OPTIONS] = index_map[button.name]
+		
 	match button.name:
-		"Party":
-			_last_focused_option = 0
 		"Fight":
-			_last_focused_option = 1
 			_change_vis_state(VisibilityState.MOVES)
 		"Run":
-			_last_focused_option = 2
 			end_battle()
-		"Item":
-			_last_focused_option = 3
-
 
 func _on_move_pressed(button: Button) -> void:
-	var num = int(button.name.trim_prefix("Button"))
-	_last_focused_move = num
+	var num := int(button.name.trim_prefix("Button"))
+	_last_focused[VisibilityState.MOVES] = num
 	
-	var move = player_actor.moves[num]
+	var move: Move = player_actor.moves[num]
 	_execute_player_turn(move)
 #endregion
