ced95a8294ccf5dfc89ec63f084b4b27bd05966a
jevon <pineapplesituation@gmail.com>
2026-04-09 16:41:14 -0700

    Fixed Bug

--- player/travel_handler.gd
+++ player/travel_handler.gd
@@ -2,6 +2,8 @@ extends Node
 
 const CLIMBING_SHOES = preload("uid://du7xft6yrfja1")
 const SURF = preload("uid://cc3o42tg3cca0")
+const CUT = preload("res://moves/cut/cut.tres")
+const ROCK_SMASH = preload("res://moves/rock_smash/rock_smash.tres")
 
 @onready var player: Player = $".."
 
@@ -38,7 +40,20 @@ func get_available_travel_methods() -> Array[Player.TravelState]:
 
 
 func get_available_removal_methods() -> Array[RemoveBlockerObject.RemovalType]:
-	return [RemoveBlockerObject.RemovalType.CUT, RemoveBlockerObject.RemovalType.SMASH]
+	var out: Array[RemoveBlockerObject.RemovalType] = []
+	if _party_has_move(CUT):
+		out.append(RemoveBlockerObject.RemovalType.CUT)
+	if _party_has_move(ROCK_SMASH):
+		out.append(RemoveBlockerObject.RemovalType.SMASH)
+	return out
+
+
+func _party_has_move(move_res: Move) -> bool:
+	for monster: Monster in Player.party.party:
+		for m in monster.moves:
+			if m == move_res:
+				return true
+	return false
 
 
 func can_surf() -> bool:
--- world_objects/interactable/blocker_object/remove_object/remove_blocker_object.gd
+++ world_objects/interactable/blocker_object/remove_object/remove_blocker_object.gd
@@ -8,6 +8,10 @@ enum RemovalType { NONE, CUT, SMASH }
 @export var removal_type: RemovalType = RemovalType.NONE
 
 
+func _ready() -> void:
+	toggle_mode(state)
+
+
 func interact(body: Player) -> void:
 	var ta: Array[String]
 	if removal_type not in body.travel.get_available_removal_methods():
@@ -19,19 +23,19 @@ func interact(body: Player) -> void:
 	ta = [question_interact_text]
 	Ui.send_text_box.emit(self, ta, false, true, false)
 	var answer = await Ui.answer_given
-	if answer:
-		match removal_type:
-			RemovalType.NONE:
-				pass
-			RemovalType.CUT:
-				toggle_mode(State.PASSABLE)
-			RemovalType.SMASH:
-				pass
+	await Ui.text_box_complete
+	if not answer:
+		return
+
+	match removal_type:
+		RemovalType.NONE:
+			pass
+		RemovalType.CUT, RemovalType.SMASH:
+			toggle_mode(State.PASSABLE)
 
 
 func toggle_mode(new_state: State) -> void:
 	state = new_state
-
 	match state:
 		State.NOT_PASSABLE:
 			collision_shape_2d.disabled = false
@@ -41,19 +45,11 @@ func toggle_mode(new_state: State) -> void:
 			visible = false
 
 
-func on_save_game(saved_data_array: Array[SavedData]) -> void:
-	var new_saved_data = SavedData.new()
-
-	new_saved_data.node_path = get_path()
-	new_saved_data.state = state as int
-
-	saved_data_array.append(new_saved_data)
-
-
 func on_load_game(saved_data_array: Array[SavedData]) -> void:
 	for data: SavedData in saved_data_array:
 		if data.node_path == get_path():
-			state = data.state as State
 			match permanence:
+				Permanence.PERMANENT:
+					toggle_mode(data.state as State)
 				Permanence.IMPERMANENT:
 					toggle_mode(State.NOT_PASSABLE)
