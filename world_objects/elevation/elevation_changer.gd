b218c81475d69e037765e33ea185de34b480bb0b
jevon <pineapplesituation@gmail.com>
2026-04-11 15:40:12 -0700

    Bridge @tool for resizing

--- world_objects/elevation/elevation_changer.gd
+++ world_objects/elevation/elevation_changer.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @export var height_level: int = 1
 
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is not TileMover:
