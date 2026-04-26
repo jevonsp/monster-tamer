4fc738913111107d7b902cb1d20378a31c8a7952
jevon <pineapplesituation@gmail.com>
2026-04-26 09:56:46 -0700

    Update graph_edge.gd

--- 3d_migration/grid_map/graph_edge.gd
+++ 3d_migration/grid_map/graph_edge.gd
@@ -1,7 +1,7 @@
 class_name GraphEdge
 extends RefCounted
 
-enum MoveKind { WALK, LEDGE_JUMP }
+enum MoveKind { WALK, LEDGE_JUMP, SURF, SLIDE }
 
 var to_cell: Vector3i = Vector3i.ZERO
 var step: Vector3i = Vector3i.ZERO
