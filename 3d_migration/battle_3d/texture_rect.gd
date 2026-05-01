550ab33eda76825b45b6963a3ac267543f91652a
jevon <pineapplesituation@gmail.com>
2026-04-30 19:52:49 -0700

    Text bug fixed

--- 3d_migration/battle_3d/texture_rect.gd
+++ 3d_migration/battle_3d/texture_rect.gd
@@ -3,5 +3,14 @@ extends TextureRect
 var actor: Monster = null
 
 
-func set_actor(a: Monster) -> void:
+func set_actor(a: Monster, update: bool = true) -> void:
 	actor = a
+	if update:
+		display()
+
+
+func display() -> void:
+	if not actor:
+		texture = null
+		return
+	texture = actor.monster_data.texture
--- 3d_migration/battle_action/actions/post_damage_text_action.gd
+++ 3d_migration/battle_action/actions/post_damage_text_action.gd
@@ -8,18 +8,21 @@ func _trigger_impl(ctx: ActionContext) -> Flow:
 	var actor: Monster = ctx.choice.actor
 	var target: Monster = last_hp_change["target"]
 	var target_fainted = last_hp_change["target_fainted"]
+	var damage: int = last_hp_change["damage"]
 	var _critical = last_hp_change["critical"]
 	var _efficacy = last_hp_change["efficacy"]
 
 	var t: String = ""
 	if ctx.chassis.is_enemy_actor(target):
 		if target_fainted:
-			t = "Foe %s fainted!\n" % target.name
-		t += "Enemy %s did %s damage!" % [actor.name, target.name]
+			t = "Foe %s fainted!" % target.name
+		else:
+			t = "Enemy %s did %d damage!" % [actor.name, damage]
 	else:
 		if target_fainted:
 			t = "Oh no! %s fainted!" % target.name
-		t += "%s did %s damage!" % [actor.name, target.name]
+		else:
+			t = "%s did %d damage!" % [actor.name, damage]
 
 	var new_ctx = ctx.fork()
 	@warning_ignore("redundant_await")
